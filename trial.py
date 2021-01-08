ae375dd2d4b0aec9157138fc85bd21eaa8c022a5
harshuop <harshwardhanxyx@gmail.com>
2021-01-08 20:19:00 +0530

    trial

--- trial.py
+++ trial.py
@@ -1,14 +1,3 @@
-def num_div(num1, num2):
-	# sol = float(num1) / float(num2)
-	# print(sol)
-	try:
-		x = (float(num1) / float(num2))
-	except ZeroDivisionError:
-		pass
-	else:
-		print(x)
-
-a = input('What is your first number? ')
-b = input('What is your second number? ')
-
-num_div(a, b)
+"""
+This is the active branch
+"""
