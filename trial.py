651d2582d7d6d3c48afd389a9ee0935ad6594298
harshuop <harshwardhanxyx@gmail.com>
2021-01-08 22:27:48 +0530

    trial

--- trial.py
+++ trial.py
@@ -1,3 +1,13 @@
-"""
-This is the active branch
-"""
+import json
+
+numbers  = [135,35,235,35,6353,35,"Hello World!"]
+file_name = 'numbers.json'
+
+with open(file_name, "w") as nums:
+    json.dump(numbers, nums)
+
+with open(file_name) as obj:
+    print(json.load(obj))
+
+
+print()
